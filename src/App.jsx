065ec2d13d92e4e5e44fb5d64337cee33e57a042
Ravi-Kiran-1318 <rkgit7767@gmail.com>
2026-03-09 09:31:38 +0530

    craeted login api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { BrowserRouter, Routes ,Route} from "react-router-dom"
-import Navbar from "./Navbar"
 import Body from "./Body"
+import Login from "./Login"
 function App() {
   return (
     <BrowserRouter basename="/">
       <Routes>
-        <Route path="/" element={<Body />} />
-        <Route path="/login" element={<h1>Login</h1>} />
-        <Route path="/signup" element={<h1>Signup</h1>} />
-
+        <Route path="/" element={<Body />}>
+          <Route path="/login" element={<Login />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
